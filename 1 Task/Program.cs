687d29216a8a1c23fe68f0e654af09129ec67ec9
DmitryStarostin <Sad12321@mail.ru>
2023-06-23 19:55:58 +0400

    Done homework

---
+++ 1 Task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b) {
+    Console.WriteLine("Первое число больше второго");
+}
+else {
+    if (a == b) {
+    Console.WriteLine("Числа равны, запустите программу еще раз (в задаче не указано требование что числа не должны быть равны)");
+    }
+    else { Console.WriteLine("Второе число больше первого");
+    }
+}
+Console.WriteLine("Конец программы");
---
+++ 4 Task/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int i=1;
+if (a == 0 || a<0) {
+    Console.WriteLine("Заданное число задано некорректно, однако в условии не указано что делать, если заданное число равно нулю или меньше нуля");
+}
+else {
+if (i == a) {
+    Console.WriteLine(i);
+}
+else {
+    Console.Write(i);
+    i++;
+while (i <= a) {
+    Console.Write($" {i}");
+    i=i+2;
+}
+Console.WriteLine("");
+}
+}
+Console.WriteLine("Конец программы");
